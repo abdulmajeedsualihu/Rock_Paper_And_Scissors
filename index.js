0ecb3a7af2242d7466c6ae746b7104dcfb7f8fd9
Abdul Majeed Sualihu <abdulmajeedsualihu2000@gmail.com>
2025-04-04 20:57:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        computer = 
+        playRound(computer, human)
+    }
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
+}
+
+
+
+console.log(playRound("You lose!, Paper beats Rock"));
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let human = prompt("Enter your choice" );
+}
+
+getHumanChoice();
+playGame();
